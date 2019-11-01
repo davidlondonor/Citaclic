a3de44c565e61baf433b539811ab9ec0151d07c1
davidlucky@gmail.com <davidlucky@gmail.com>
2019-11-01 17:47:31 -0500

    creacion pagina doctores y comienzo de pagina horario

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
+import React from "react";
+import Menu from "./home/menu";
+import Footer from "./home/footer";
+import Home from "./home/Home";
+import { Switch, Route } from "react-router-dom";
+import SignUP from "./home/signUP/SignUP";
+import Cards from "./components/cards";
 
-import React from 'react';
-import Menu from './home/menu';
-import Footer from './home/footer';
-import Home from './home/Home';
-import {Switch, Route } from 'react-router-dom';
-import SignUP from './home/signUP/SignUP';
+import "./App.css";
 
-import './App.css';
+const Error = () => {
+  return <div>ERROR</div>;
+};
 
-const Error = () =>{
-  return(
-    <div>ERROR</div>
-  )
-}
- 
 const App = () => {
   return (
-    <div className="App">
-        <Menu />
-        <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/registro' component={SignUP}/>
-            <Route component={Error}/>
-        </Switch>
-        <Footer />  
-     </div>
-
+    <div className='App'>
+      <Menu />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/registro' component={SignUP} />
+        <Route component={Error} />
+      </Switch>
+      <Cards />
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -6,15 +6,33 @@ class Cards extends Component {
   state = {};
   render() {
     return (
-      <div className='container cardDoc'>
-        <div className='photoDoc'>
+      <div className='containerDoc'>
+        <div className='cardCards'></div>
+        <div className='photoCards'>
           <img src='https://i.ibb.co/gT0S0KG/foto-4.png' alt=''></img>
         </div>
-        <div className='detailsDoc'>
-          <h4 className='nameDoc'>Nombre del Doc</h4>
-          <h6 className='profesionalDoc'>Profesion Doc</h6>
-          <h6 className='locatedDoc'>Ubicacion</h6>
-          <p className='starDoc'>
+        <div className='detailsDCards'>
+          <h4 className='nameCards'>Nombre del Doc</h4>
+          <h6 className='profesionalCards'>Profesion Doc</h6>
+          <h6 className='locatedCards'>Ubicacion</h6>
+          <p className='starCards'>
+            <MdStarBorder />
+            <MdStarBorder />
+            <MdStarBorder />
+            <MdStarBorder />
+            <MdStarBorder />
+          </p>
+        </div>
+
+        <div className='cardCards'></div>
+        <div className='photoCards'>
+          <img src='https://i.ibb.co/gT0S0KG/foto-4.png' alt=''></img>
+        </div>
+        <div className='detailsDCards'>
+          <h4 className='nameCards'>Nombre del Doc</h4>
+          <h6 className='profesionalCards'>Profesion Doc</h6>
+          <h6 className='locatedCards'>Ubicacion</h6>
+          <p className='starCards'>
             <MdStarBorder />
             <MdStarBorder />
             <MdStarBorder />
