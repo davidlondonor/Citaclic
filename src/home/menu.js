a25823351d4e1359395b0b442114e94e88056f13
Débora Soto <soto.deboram@gmail.com>
2019-11-04 17:21:42 -0500

    variosCambios

--- src/home/menu.js
+++ src/home/menu.js
@@ -5,6 +5,7 @@ const Menu = () => {
     return (
         <div className="container">
             <div className="menu">
+                <img className="img-logo" src="https://i.ibb.co/2Pzv5g8/logo.png" alt="logo" />
                 <a href="#citaclic" className="nav">¿Por qué citaclic?</a>
                 <a href="#nosotros" className="nav">Nosotros</a>
                 <a href="#ayuda" className="nav">Ayuda</a>
--- src/home/signUP/SignUP.js
+++ src/home/signUP/SignUP.js
@@ -1,36 +1,109 @@
 import React from 'react';
 import './SignUP.css';
 
-const SignUP = () => {
-    return(
+class SignUP extends React.Component{
+    state={
+        isDirty: false,
+        name: "",
+        isNameEmpty: false,
+        lastName: "",
+        isLastNameEmpty: false,
+        email: "",
+        isEmailValue: false,
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+    handleNameChange = (event) => {
+        const nameValue = event.target.value;
+        this.setState({
+            isDirty: true,
+            isNameEmpty: nameValue === "",
+            name: nameValue
+        })
+    }
+    handleLastNameChange = (event) => {
+        const lastNameValue = event.target.value;
+        this.setState({
+            isDirty: true,
+            isLastNameEmpty: lastNameValue === "",
+            lastName: lastNameValue
+        })
+    }
+    handleEmailChange= (event) => {
+        const emailValue= event.target.value;
+
+        const validateEmail = (email) => {
+            return /\S+@\S+\.\S+/.test(email)
+        }; 
+
+        this.setState({
+            isDirty: true,
+            isEmailValue: validateEmail(emailValue),
+            email: emailValue
+        })
+    }
+    render(){
+        return(
         <div className="container container-signup">
             <img className="img-signup" src="https://place-hold.it/751x660"></img>
                <div className="right-container"> 
                 <div className="SignUP">
-                    <form className="form">
+                    <form onSubmit={this.handleSubmit} className="form" autoComplete="off">
                         <div className="name">
-                            <label className="text-label">Nombre</label>
-                            <input className="stylesInput" type="text"></input>
+                            <label htmlFor="nombre" className="text-label">Nombre</label>
+                            <input
+                                className="stylesInput" 
+                                type="text"
+                                autoFocus
+                                id="nombre"
+                                value={this.state.name}
+                                onChange={this.handleNameChange}
+                                style={this.state.isDirty && this.state.isNameEmpty ? { border: '1px solid red' } : {}}
+                             />
+                            {this.state.isDirty && this.state.isNameEmpty ? 
+                            (<p className="campoVacio">El campo no puede estar vacío</p> ): null}
                         </div>
                         <div className="lastName">
-                            <label className="text-label">Apellido</label>
-                            <input className="stylesInput" type="text"></input>
+                            <label htmlFor="apellido" className="text-label">Apellido</label>
+                            <input 
+                                className="stylesInput" 
+                                type="text"
+                                id="apellido"
+                                value={this.state.lastName}
+                                onChange={this.handleLastNameChange}
+                                style={this.state.isDirty && this.state.isLastNameEmpty ? { border: '1px solid red' } : {}}
+                            />
+                            {this.state.isDirty && this.state.isLastNameEmpty ?
+                            (<p className="campoVacio">El campo no puede estar vacío</p>): null}
                         </div>
                         <div className="email">
-                            <label className="text-label">Correo electrónico</label>
-                            <input className="stylesInput" type="email"></input>
+                            <label htmlFor="email" className="text-label">Correo electrónico</label>
+                            <input 
+                                className="stylesInput" 
+                                type="text"
+                                id="email"
+                                value={this.state.email}
+                                onChange={this.handleEmailChange}
+                                style={this.state.isDirty && !this.state.isEmailValue ? { border: '1px solid red' } : {}}
+                            />
+                                {this.state.isDirty && !this.state.isEmailValue ?
+                            (<p className="campoVacio">Debe ingresar un correo electrónico válido</p>): null}
                         </div>
                         <div className="password">
                             <label className="text-label">Contraseña</label>
+                            <input className="stylesInput contraseña-margin" type="password"></input>
+
+                            <label className="text-label">Confirmar contraseña</label>
                             <input className="stylesInput" type="password"></input>
                         </div>
-                        <div className="start-boton">
-                            <button className="boton" type="submit">Registrarme</button>
-                        </div>
+                            <button disabled={this.state.isNameEmpty || this.state.isLastNameEmpty} className="boton-signUp" type="submit">Registrarme</button>
                     </form>
                 </div>
              </div>  
       </div>
-    )
+        )
+    }
 }
 export default SignUP;
