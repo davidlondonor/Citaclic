923f958cc5b4b07737ad05286b27476aa8ada7c4
davidlucky@gmail.com <davidlucky@gmail.com>
2019-11-10 21:27:04 -0500

    Modificacion del home, ultimos detalles previo a la entrega

--- src/home/menu.js
+++ src/home/menu.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import './menu.css';
-
+import React from "react";
+import "./menu.css";
 
 const Menu = () => {
-    return (
-        <div className="container">
-            <div className="menu">
-                <a href="#citaclic" className="nav">¿Por qué citaclic?</a>
-                <a href="#nosotros" className="nav">Nosotros</a>
-                <a href="#ayuda" className="nav">Ayuda</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className='container'>
+      <div className='menu'>
+        <a href='#citaclic' className='nav'>
+          ¿Por qué citaclic?
+        </a>
+        <a href='#nosotros' className='nav'>
+          Nosotros
+        </a>
+        <a href='#ayuda' className='nav'>
+          Ayuda
+        </a>
+      </div>
+    </div>
+  );
 };
 export default Menu;
