4e4c5bfa1ae1e4cf1958cd42cf250ab9d9d6cac4
davidlucky@gmail.com <davidlucky@gmail.com>
2019-11-13 13:38:25 -0500

    arregle los logos del modal

--- src/tarjetaDoctor/Hour.jsx
+++ src/tarjetaDoctor/Hour.jsx
@@ -2,19 +2,36 @@ import React from "react";
 import { MdChevronRight } from "react-icons/md";
 import "./hour.css";
 import ModalPagoCita from "./ModalPagoCita";
+import pse from "../../src/assets/pse.png";
+import visa from "../assets/visa.png";
+import masterCard from "../assets/masterCard.png";
+import nequi from "../assets/nequi.svg";
 
 const Hour = ({ horaInicio, horaFinal, handleFunction, state }) => {
   return (
-    <div className='scheduleHour'>
-      <div className='firstHour'>{horaInicio}</div>
-      <MdChevronRight className='iconHour' />
-      <div className='firstHour'>{horaFinal}</div>
-      <button className='buttonHour' type='button' onClick={handleFunction}>
+    <div className="scheduleHour">
+      <div className="firstHour">{horaInicio}</div>
+      <MdChevronRight className="iconHour" />
+      <div className="firstHour">{horaFinal}</div>
+      <button className="buttonHour" type="button" onClick={handleFunction}>
         {state.isOpen ? "Close" : "Open"}
       </button>
       {state.isOpen && (
-        <ModalPagoCita handleOnClick={handleFunction}>
-          <h1>Ventana de Pagos</h1>
+        <ModalPagoCita
+          className="estiloModalPagoCita"
+          handleOnClick={handleFunction}
+        >
+          <h1 className="tituloModal">Realiza tu pago en</h1>
+          <div className="logoContainer">
+            <img className="logoModal" src={pse} alt="Logo PSE" />
+            <img className="logoModal visa" src={visa} alt="Logo Visa" />
+            <img
+              className="logoModal"
+              src={masterCard}
+              alt="Logo Master Card"
+            />
+            <img className="logoModal" src={nequi} alt="Logo Nequi" />
+          </div>
         </ModalPagoCita>
       )}
     </div>
--- src/tarjetaDoctor/tarjetaDocs.jsx
+++ src/tarjetaDoctor/tarjetaDocs.jsx
@@ -15,36 +15,36 @@ class TarjetaDoc extends Component {
 
   render() {
     return (
-      <div className='containerDoc'>
-        <div className='containerappointment'>
-          <div className='cardCardsDoc'>
-            <div className='photoCardsDoc'>
+      <div className="containerDoc">
+        <div className="containerappointment">
+          <div className="cardCardsDoc">
+            <div className="photoCardsDoc">
               <img
-                src='https://i.ibb.co/gT0S0KG/foto-4.png'
-                alt='un texto'
+                src="https://i.ibb.co/gT0S0KG/foto-4.png"
+                alt="un texto"
               ></img>
             </div>
-            <div className='detailsDCards'>
-              <h4 className='nameCardsDoc'>Manuela Arango</h4>
-              <h6 className='profesionalCardsDoc'>Dermartóloga</h6>
-              <h6 className='locatedCardsDoc'>Poblado</h6>
+            <div className="detailsDCards">
+              <h4 className="nameCardsDoc">Manuela Arango</h4>
+              <h6 className="profesionalCardsDoc">Dermartóloga</h6>
+              <h6 className="locatedCardsDoc">Poblado</h6>
               <div>
-                <MdStarBorder className='starCardsDoc' />
-                <MdStarBorder className='starCardsDoc' />
-                <MdStarBorder className='starCardsDoc' />
-                <MdStarBorder className='starCardsDoc' />
-                <MdStarBorder className='starCardsDoc' />
+                <MdStarBorder className="starCardsDoc" />
+                <MdStarBorder className="starCardsDoc" />
+                <MdStarBorder className="starCardsDoc" />
+                <MdStarBorder className="starCardsDoc" />
+                <MdStarBorder className="starCardsDoc" />
               </div>
             </div>
             {/* <PerfilDoctores /> */}
           </div>
         </div>
 
-        <div className='appointment'>
-          <h3 className='avalibleDoc'>Disponibilidad</h3>
-          <p className='priceDoc'>Valor de la consulta</p>
+        <div className="appointment">
+          <h3 className="avalibleDoc">Disponibilidad</h3>
+          <p className="priceDoc">Valor de la consulta</p>
           <Day />
-          <div className='hourDocs'>
+          <div className="hourDocs">
             <Hour
               state={this.state}
               handleFunction={this.handleOnClick}
